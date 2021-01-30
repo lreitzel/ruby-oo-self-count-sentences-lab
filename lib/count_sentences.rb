be6843a20b30c5521817b770b48e5016a7f39afc
Lydia <lreitzel@gmail.com>
2021-01-30 12:36:50 -0800

    commit tree 98dab955b1b69cf3dc46c200488070fbce7a95ee

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
     
   end
 
   def question?
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
 
   end
 
   def exclamation?
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
 
   end
 
   def count_sentences
-
+    self.split(/\?|\.|\!/).delete_if{|array| array.size < 1}.count
   end
 end
